1361aa1507f67b682b66c61ec70901136302720e
thoom76 <thomas.smeele@student.hu.nl>
2019-04-14 21:10:29 +0200

    Set-up the library for uart & autocomplete + started writing some beginning code

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,17 @@
 #include "hwlib.hpp"
+#include "hardware_usart.hpp"
+
 
 int main(void) {
   // kill the watchdog
-    WDT->WDT_MR = WDT_MR_WDDIS;
-    hwlib::wait_ms(1000);
-    for (;;){
-        hwlib::cout << "this works via arduino";
-        hwlib::wait_ms(1000);
+  WDT->WDT_MR = WDT_MR_WDDIS;
+
+  auto usart_port = r2d2::uart_ports_c::uart1;
+  auto uart = r2d2::hardware_usart_c(230400, usart_port);
+
+  for(;;){
+    if(uart.available() > 0) {
+      hwlib::cout << "Received from real UART: " << (int)uart.receive() << hwlib::endl;
     }
+  }
 }
