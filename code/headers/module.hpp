b00f1db7257adc986c8bcc014c29915a3fd2eede
ThePatman3 <patrick.vanderbend@student.hu.nl>
2019-06-19 13:54:15 +0200

    Added doxygen documentation

--- code/headers/module.hpp
+++ code/headers/module.hpp
@@ -5,6 +5,11 @@
 
 // This is the main module class that measures the smallest distance and sends it on the canbus
 namespace r2d2::distance {
+
+    /// \brief The main class for the distance module.
+    /// \detail
+    /// This class uses a lidar to measure the distance between the lidar and the closest object.
+    /// When a distance request has been received this class sends the smallest detected distance over the can-bus.
     class module_c : public base_module_c {
         private:
             lidar_c lidar;
@@ -14,7 +19,8 @@ namespace r2d2::distance {
             bool smallest_value_set_first_time = false;
 
         public:
-
+            /// \brief r2d2::distance::module_c constructor
+            /// \detail Constructs a r2d2::distance::module_c object that will use the given comm and lidar.
             module_c(base_comm_c &comm, lidar_c &lidar)
                 : base_module_c(comm),
                   lidar(lidar) {
@@ -22,6 +28,11 @@ namespace r2d2::distance {
                 comm.listen_for_frames({r2d2::frame_type::DISTANCE});
             }
 
+            /// \brief This modules process function that allows the module to process (input)data.
+            /// \detail
+            /// In this function the module first checks for distance request frames. If a distance request frame has been received,
+            /// the module will update its distance data with input from the lidar. It then searches for the smallest distance
+            /// and sends this distance over the can-bus in a frame_distance_s packet.
             void process() override {
                 while(comm.has_data()) {
                     auto frame = comm.get_data();
