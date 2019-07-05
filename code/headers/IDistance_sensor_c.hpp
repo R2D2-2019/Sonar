3e321fb220597b68db6833fe3e12923c6a212e89
ThePatman3 <patrick.vanderbend@student.hu.nl>
2019-07-05 14:14:19 +0200

    Added doxygen documentation
    Doxygen was added for the module_c and IDistance_sensor_c classes.

--- code/headers/IDistance_sensor_c.hpp
+++ code/headers/IDistance_sensor_c.hpp
@@ -5,9 +5,16 @@
 
 namespace r2d2::distance
 {
+    /// \brief
+    /// An interface for classes that represent a distance sensor.
+    /// \detail
+    /// This interface is used by module_c as the distance sensor.
     class IDistance_sensor_c
     {
     public:
+        /// \brief
+        /// Function that fills a frame with the measured distances.
+        /// \param frame A reference to the frame that will be filled.
         virtual void fill_distance_frame(frame_distance_s & frame) = 0;
     };
 } // R2D2::Distance
--- code/headers/module.hpp
+++ code/headers/module.hpp
@@ -5,17 +5,35 @@
 
 
 namespace r2d2::distance {
+    /// \brief
+    /// The module class of the distance module.
+    /// \detail
+    /// In the process function, this module checks if a request of type DISTANCE is received,
+    /// if such a request is received the module will use its distance sensor to measure the distance
+    /// in as many directions and send it over the canbus in a frame_distance_s frame.
+    /// The distance sensor this module uses can be easily replaced with another distance sensor,
+    /// as this class has a reference to an IDistance_sensor_c for its distance sensor, which is an interface.
     class module_c : public base_module_c {
     private:
         IDistance_sensor_c & distance_sensor;
         frame_distance_s distance_frame;
     public:
+        /// \brief
+        /// The constructor of the distance module_c class.
+        /// \param comm The comm that will be used to communicate on the canbus.
+        /// \param distance_sensor The distance sensor that will be used.
         module_c(base_comm_c &comm, IDistance_sensor_c & distance_sensor)
             : base_module_c(comm), distance_sensor(distance_sensor), distance_frame() {
 
             comm.listen_for_frames({r2d2::frame_type::DISTANCE});
         }
 
+        /// \brief
+        /// The process function of this module.
+        /// \detail
+        /// In this function the module checks for a DISTANCE request on the canbus.
+        /// If such a request has been received, the module will use the distance sensor
+        /// to measure the distance and send it on the canbus in a frame_distance_s frame.
         void process() override {
             while (comm.has_data()) {
                 auto frame = comm.get_data();
